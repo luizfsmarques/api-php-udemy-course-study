b306c414dbfcf833e732be98f1164f1878d41a0c
Luiz Marques <l.fernando.9.lfsm@gmail.com>
2024-12-11 00:52:25 -0300

    section 2 - class 7 - decoding json

--- section2/index.php
+++ section2/index.php
@@ -5,6 +5,22 @@
 */
 
 // $response = file_get_contents("https://example.com");
-$response = file_get_contents("https://randomuser.me/api");
+// $response = file_get_contents("https://randomuser.me/api");
+// echo $response;
 
-echo $response;
+/*
+    Class 6
+    Decoding the JSON response in PHP object and in an associative array
+*/
+
+// $response = file_get_contents("https://randomuser.me/api");
+
+// $data = json_decode($response); //to PHP object
+// $data = json_decode($response,true); //to associative array
+
+// var_dump($data);
+// echo $data['results'][0]['name']['first'],"\n";
+
+/*
+    Class 7
+*/

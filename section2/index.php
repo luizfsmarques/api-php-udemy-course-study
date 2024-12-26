a8849e4296ba922b5f4a30c7cfce7d94d5578164
Luiz Marques <l.fernando.9.lfsm@gmail.com>
2024-12-26 01:14:27 -0300

    section2, class 7 - Using the response of an API into a view page

--- section2/index.php
+++ section2/index.php
@@ -23,4 +23,44 @@
 
 /*
     Class 7
+    We got a name from a form, after called an api, decoded the JSON response and at the 
+    end displayed the result on page.
+
+    Obs.: Don't forget that you using the open tag <?php, at the beggining of the file.
 */
+
+if(!empty($_GET['name']))
+{
+    $response = file_get_contents("https://api.agify.io?name={$_GET['name']}");
+    $data = json_decode($response,true);
+    $name = $data['name'];
+    $age = $data['age'];
+}
+
+?>
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Example</title>
+    </head>
+    <body>
+
+        <?php if(isset($name) && isset($age)): ?>
+            Name:<?= $name ?>
+            </br>
+            Age: <?= $age ?>
+        <?php endif; ?>
+        
+        </br>
+        </br>
+        <form>
+            <label for="name">Name</label>
+            <input name="name" id="name">
+            <button>Guess age</button>
+        </form>
+    </body>
+
+
+</html>
